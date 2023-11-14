137919cea95a77f6bb971d6465907ced989f9184
Jack Brewer <Jack_brewer96@hotmail.com>
2023-11-14 15:10:02 +0000

    Added tags files

---
+++ OWN/Source/OWN/Tags/OWNTags.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OWN/Tags/OWNTags.h"
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_Damage, "Gameplay.Damage");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_DamageImmunity, "Gameplay.DamageImmunity");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_DamageSelfDestruct, "Gameplay.Damage.SelfDestruct");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_FellOutOfWorld, "Gameplay.Damage.FellOutOfWorld");
+UE_DEFINE_GAMEPLAY_TAG(TAG_OWN_Damage_Message, "OWN.Damage.Message");
---
+++ OWN/Source/OWN/Tags/OWNTags.h
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Gameplay_Damage);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Gameplay_DamageImmunity);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Gameplay_DamageSelfDestruct);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Gameplay_FellOutOfWorld);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_OWN_Damage_Message);
